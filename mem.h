d9fa3dcec7448145011427bc98aeb110aaf32bb0
wuyefeng <wuyefeng1975@gmail.com>
2016-12-02 11:46:08 -0500

    fix bug in mem.h

--- mem.h
+++ mem.h
@@ -10,7 +10,7 @@ extern void *Mem_calloc( long count, long nbytes, const char *file, int line );
 extern void *Mem_copy( void* destination, void* source, long nbytes, const char *file, int line );
 
 #define ALLOC(nbytes) Mem_alloc((nbytes), __FILE__, __LINE__)
-#define CALLOC(count, nbytes) Mem_alloc((count), (nbytes), __FILE__, __LINE__)
+#define CALLOC(count, nbytes) Mem_calloc((count), (nbytes), __FILE__, __LINE__)
 #define COPY(destination, source, nbytes) Mem_copy((destination), (source), (nbytes), __FILE__, __LINE__)
 #define NEW(p) ((p) = ALLOC((long)sizeof *(p)))
 #define NEW0(p) ((p) = CALLOC(1, (long)sizeof *(p)))
