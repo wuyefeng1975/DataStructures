050d8e42e8fd6299d82d65310c1d281d9d58202d
wuyefeng <wuyefeng1975@gmail.com>
2017-03-26 17:30:01 -0400

    Add some common function

--- utility_integer.c
+++ utility_integer.c
@@ -1,5 +1,7 @@
+#include "utility_integer.h"
 #include <sys/time.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 long get_random_integer( long min, long max )
 {
@@ -7,4 +9,23 @@ long get_random_integer( long min, long max )
 	static int i = 0;
 	srand(time(&t) + i++);
 	return rand() % ( max - min + 1 ) + min;
+}
+
+int random_integer(int min, int max)
+{
+	return rand() % ( max - min + 1 ) + min;
+}
+
+void print_int_array(int *arr, int size)
+{
+    for( int i = 0; i < size; i++ )
+        printf( "%d\t", arr[i] );
+    printf( "\n" );    
+}
+
+void swap_int_value(int *value1, int *value2)
+{
+    int temp = *value1;
+    *value1 = *value2;
+    *value2 = temp;
 }
--- utility_integer.h
+++ utility_integer.h
@@ -1,4 +1,7 @@
 #ifndef _H_UTILITY_INTEGER
 #define _H_UTILITY_INTEGER
-    long get_random_integer(long min, long max);
+    extern long get_random_integer(long min, long max);
+    extern int random_integer(int min, int max);
+    extern void print_int_array(int *arr, int size);
+    extern void swap_int_value(int *value1, int *value2);
 #endif
